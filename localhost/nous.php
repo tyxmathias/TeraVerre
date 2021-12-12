54f3b30209a6b47359e0149ca25860a0b33e714c
Mathias Tyxmat <mathias21c@gmail.com>
2021-12-12 23:22:54 +0100

    A propos

---
+++ localhost/nous.php
@@ -0,0 +1,23 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<?php include 'head.php' ?>
+<body>
+
+<?php include 'header.php' ?>
+
+<div class="container">
+    <h2>Qui sommes nous ?</h2>
+    <div class="row">
+        <div class="col mt-5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse et iste maxime nihil officiis qui quod ratione rem repellat, sequi! Amet consectetur debitis dolores dolorum et incidunt nemo nihil sint.
+        </div>
+        <div class="col">
+            <img src="images/nous.png">
+        </div>
+    </div>
+</div>
+<?php include 'footer.php' ?>
+</body>
+</html>
